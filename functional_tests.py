02ce189c9c9dfce5badf337fc56438e8a0e37fbc
Sergiy Tsymbal <tcymbal@gmail.com>
2015-02-15 01:05:55 +0200

    Part of the user story

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 
+#Edith has heard about a cool new online to-do app
+#She goes to check out its homepage
 browser = webdriver.Chrome()
 browser.get('http://localhost:8000')
 
