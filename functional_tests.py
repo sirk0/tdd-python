1f2e598a261b8ada58536540a9a81a2ea7d7311e
Sergiy Tsymbal <tcymbal@gmail.com>
2015-02-22 00:29:52 +0200

    'identation'

--- functional_tests.py
+++ functional_tests.py
@@ -4,55 +4,55 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
-	def setUp(self):
-		self.browser = webdriver.Chrome()
-		self.browser.implicitly_wait(3)
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
 
-	def tearDown(self):
-		self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		#Edith has heard about a cool new online to-do app
-		#She goes to check out its homepage
-		self.browser.get('http://localhost:8000')
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #Edith has heard about a cool new online to-do app
+        #She goes to check out its homepage
+        self.browser.get('http://localhost:8000')
 
-		# She notices the page title and heade mention to-d- list
-		self.assertIn('To-Do', self.browser.title)
-		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('To-Do', header_text)
+        # She notices the page title and heade mention to-d- list
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-		# She is invited to enter a to-do item straight away
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertEqual(inputbox.get_attribute('placeholder'), 
-			'Enter a to-do item')
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 
+            'Enter a to-do item')
 
-		# She types "Buy peacocks feathers" into a text box 
-		# (Edith's hobby is tying fly-fishing lures)
-		inputbox.send_keys('Buy peacock feathers')
+        # She types "Buy peacocks feathers" into a text box 
+        # (Edith's hobby is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
-		# When she hits enter, the page updates, and now the page lists
-		# "1: Buy peacocks feathers" sa an item in to-do list
-		inputbox.send_keys(Keys.ENTER)
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacocks feathers" sa an item in to-do list
+        inputbox.send_keys(Keys.ENTER)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacocks feather' for row in rows), 
-			'New to-do item did not appear in table')
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacocks feather' for row in rows), 
+            'New to-do item did not appear in table')
 
-		# There is still a text box inviting her to add another item. 
-		# She enters "Use pacocks feathers to make a fly" (Edith is very methodical)
-		self.fail('Finish the test')
+        # There is still a text box inviting her to add another item. 
+        # She enters "Use pacocks feathers to make a fly" (Edith is very methodical)
+        self.fail('Finish the test')
 
-		# The page updates again, and now shows both items on her list
+        # The page updates again, and now shows both items on her list
 
-		# Edith wonders whether site will remember her list. Then she sees
-		# that the site has generated a uniques URL for her -- there is some
-		# explanatory text to that effect
+        # Edith wonders whether site will remember her list. Then she sees
+        # that the site has generated a uniques URL for her -- there is some
+        # explanatory text to that effect
 
-		# She vsists site URL - her to-do list is still here.
+        # She vsists site URL - her to-do list is still here.
 
-		#Satisfied, she goes to sleep
+        #Satisfied, she goes to sleep
 
 if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+    unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -7,12 +7,12 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
 
-	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = home_page(request)
-		expected_html = render_to_string('home.html')
-		self.assertEqual(response.content.decode(), expected_html)
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
